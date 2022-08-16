51d48848bb3c82bb92fd148629058e96a5079573
shaik-aaron <shaikaaronbbhs@gmail.com>
2022-08-16 12:40:13 +0530

    adding dist

--- README.md
+++
@@ -1,3 +0,0 @@
-# Digital Business Card
-
-A small React Project from scrimba's free React course
--- src/index.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom";
-import BusinessCard from "./Components/BusinessCard"
-
-ReactDom.render(<BusinessCard/>,document.getElementById('root'))
