1bf9bc1f3d2776c10475e02264dd700f435674e0
shaik-aaron <shaikaaronbbhs@gmail.com>
2022-08-16 12:17:52 +0530

    first commit

--- src/Components/Email.jsx
+++ src/Components/Email.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
+function Message() {
+    alert("It's an Email!");
+}
+
 export default function Email() {
     return (
-        <button type="button" className="EmailButton">
-            <i class="fa fa-envelope" aria-hidden="true"></i>
+        <button type="button" className="EmailButton" onClick={Message}>
+            <i className="fa fa-envelope" aria-hidden="true"></i>
             <p>Email</p>
         </button>
     )
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,11 +3,11 @@ import React from "react";
 export default function Footer() {
     return (
         <footer>
-            <a href=""><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
-            <a href=""><i class="fa fa-facebook fa-2x" aria-hidden="true"></i></a>
-            <a href=""><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a>
-            <a href=""><i class="fa fa-linkedin fa-2x" aria-hidden="true"></i></a>
-            <a href=""><i class="fa fa-github fa-2x" aria-hidden="true"></i></a>
+            <a href=""><i className="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
+            <a href=""><i className="fa fa-facebook fa-2x" aria-hidden="true"></i></a>
+            <a href=""><i className="fa fa-instagram fa-2x" aria-hidden="true"></i></a>
+            <a href=""><i className="fa fa-linkedin fa-2x" aria-hidden="true"></i></a>
+            <a href=""><i className="fa fa-github fa-2x" aria-hidden="true"></i></a>
         </footer>
     )
 }
